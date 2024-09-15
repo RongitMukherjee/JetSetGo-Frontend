4dbd6461f46c092914880f80e1386dc668a429f8
Rongit <86566634+RongitMukherjee@users.noreply.github.com>
2024-09-16 02:06:21 +0530

    Made the site more interactive
    1. Changed multi city to both way; on clicking one way it shows only departure while on clicking both way it also shows departure and return
    2. Added validation and submit response to form
    3. Added auto play video to Ultimate Travel Spots
    4. Added carousel to gallery section
    5. Added hover effects to like and share buttons, capability to like and unlike also added

---
+++ index.js
@@ -0,0 +1,98 @@
+document.querySelectorAll('.type').forEach(typeElement => {
+    const img = typeElement.querySelector('img');
+    const text = typeElement.querySelector('.textc');
+
+    typeElement.addEventListener('mouseover', () => {
+      const src = img.getAttribute('src');
+      img.setAttribute('src', src.replace('blue', 'white'));
+      text.style.color = 'white';
+    });
+
+    typeElement.addEventListener('mouseout', () => {
+      const src = img.getAttribute('src');
+      img.setAttribute('src', src.replace('white', 'blue'));
+      text.style.color = 'rgb(28, 89, 150)';
+    });
+  });
+
+  $("input[name = 'city']").change(function() {
+    var selected = $("input[name = 'city']:checked").val();
+    console.log(selected);
+
+    if(selected === "two") {
+        $(".ret").css("display","block");
+        $(".retvr").css("display","block");
+    }
+    else {
+        $(".ret").css("display","none");
+        $(".retvr").css("display","none");
+    }
+  });
+
+  function validateForm() {
+    var validRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (document.getElementById("floatingName").value === "") {
+      alert("Please enter name!");
+      return false;
+    }
+    else if (document.getElementById("floatingEmail").value === "") {
+      alert("Please enter email!");
+      return false;
+    }
+    else if (!document.getElementById("floatingEmail").value.match(validRegex)) {
+      alert("Incorrect email format!");
+      return false;
+    }
+    else if (document.getElementById("floatingMessage").value === "") {
+      alert("Please enter a message!");
+      return false;
+    }
+    else {
+      alert("Submitted! Thank you for your message!");
+      $(".contactform").trigger("reset");
+    }
+  }
+
+  $(".indcard").mouseenter(function() {
+    $(this).find(".vid").css("display", "block");
+    $(this).find("img").css("display", "none");
+    var video = $(this).find('video').get(0);
+    video.play();
+  })
+
+  $(".indcard").mouseleave(function() {
+    var video = $(this).find('video').get(0);
+    video.pause();
+    video.currentTime = 0;
+    $(".vid").css("display", "none");
+    $("img").css("display", "block");
+  })
+
+  $(".heart").click(function() {
+    var source = $(this).text();
+    if (source == "♡") {
+      $(this).text("♥︎");
+      $(this).siblings(".hovertext-heart").text("Unike").css("right","29px");
+    }
+    else {
+      $(this).text("♡");
+      $(this).siblings(".hovertext-heart").text("Like").css("right","34px");
+    }
+  })
+
+  $(".heart").mouseenter(function() {
+    $(this).siblings(".hovertext-heart").show();
+  })
+
+  $(".heart").mouseleave(function() {
+    $(this).siblings(".hovertext-heart").hide();
+  })
+
+  $(".share").mouseenter(function() {
+    $(this).siblings(".hovertext-share").show();
+  })
+
+  $(".share").mouseleave(function() {
+    $(this).siblings(".hovertext-share").hide();
+  })
